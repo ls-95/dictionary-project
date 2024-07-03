b5bef858ffc5e1bc6708daebe8884f442cbc7d22
ls-95 <143622355+ls-95@users.noreply.github.com>
2024-07-03 10:46:59 +0200

    Added alert for word entered in search input

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 import explore from "./explore.jpg";
 
 export default function App() {
@@ -6,7 +7,26 @@ export default function App() {
     <div className="App">
       <header className="App-header">
         <img src={explore} className="App-logo img-fluid" alt="logo" />
+        <h1>Welcome</h1>
+        <h3>What word do you want to look up?</h3>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        This project was coded by{" "}
+        <a href="https://github.com/ls-95" target="_blank" rel="noreferrer">
+          Laetitia Saunders
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/ls-95/dictionary-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
