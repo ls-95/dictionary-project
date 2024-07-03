9f4d66d2884c7a6cd333ec3e5cfc841861e5cdd1
ls-95 <143622355+ls-95@users.noreply.github.com>
2024-07-03 09:17:43 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import explore from "./explore.jpg";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={explore} className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
