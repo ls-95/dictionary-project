74614d02d0f5f4c236e34b5afd1074b0777b7e08
ls-95 <143622355+ls-95@users.noreply.github.com>
2024-07-08 10:06:51 +0200

    Added phonetic audio and text

---
+++ src/Phonetic.js
@@ -0,0 +1,21 @@
+export default function Phonetic(props) {
+  //console.log(props.phonetic);
+  if (props.phonetic.audio.length > 1) {
+    return (
+      <div className="Phonetics">
+        <a href={props.phonetic.audio} target="_blank">
+          Listen
+        </a>
+        <p>{props.phonetic.text}</p>
+      </div>
+    );
+  } else if (props.phonetic.text) {
+    return (
+      <div className="Phonetics">
+        <p>{props.phonetic.text}</p>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
--- src/Results.js
+++ src/Results.js
@@ -1,14 +1,22 @@
 import Meaning from "./Meaning";
 import GeneralSynonyms from "./GeneralSynonyms";
+import Phonetic from "./Phonetic";
 import "./Results.css";
 
 export default function Results(props) {
-  //console.log(props.info.meanings[0].synonyms);
-
   if (props.info) {
+    //console.log(props.info.meanings[0].synonyms);
+    console.log(props.info);
     return (
       <div className="Results">
         <h2>{props.info.word}</h2>
+        {props.info.phonetics.map(function (phonetics, index) {
+          return (
+            <div key={index}>
+              <Phonetic phonetic={phonetics} />
+            </div>
+          );
+        })}
         <h4>Synonyms: </h4>
         <GeneralSynonyms words={props.info.meanings[0].synonyms} />
         {props.info.meanings.map(function (meaning, index) {
